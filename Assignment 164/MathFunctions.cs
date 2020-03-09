068fe0da4d83bc481292c144d8030bfb7001af15
Richard <40587587+Quetives@users.noreply.github.com>
2020-03-09 09:07:32 -0700

    Add project files.

---
+++ Assignment 164/MathFunctions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_164
+{
+    class MathFunctions
+    {
+        public void divided(in int int1, out int answer)
+        {
+            answer = int1 / 2;
+        }
+        public void numbers(out int num1, out int num2)
+        {
+            num1 = 1;
+            num2 = 2;
+        }
+        public void numbers(int num1, int num2, out int num3)
+        {
+            num3 = num1 + num2;
+        }
+    }
+}
---
+++ Assignment 164/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_164
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1;
+            int answer;
+            MathFunctions functionOne = new MathFunctions();
+
+            Console.WriteLine("Please enter a number.");
+            num1 = Convert.ToInt32(Console.ReadLine());
+            functionOne.divided(num1, out answer);
+            Console.WriteLine(num1 + " divided by 2 equals:" + answer);
+
+            int num2;
+            int num3;
+            
+            MathFunctions functionTwo = new MathFunctions();
+            functionTwo.numbers(out num2, out num3);
+            Console.WriteLine(num2 + ", " + num3);
+
+            int int1 = 5;
+            int int2 = 5;
+            int total;
+            MathFunctions functionThree = new MathFunctions();
+            functionThree.numbers(int1, int2, out total);
+            Console.WriteLine(int1 + " plus " + int2 + " equals " + total);
+            Console.ReadLine();
+        }
+    }
+}
